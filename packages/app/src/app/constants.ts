cf5950fb9775e7d5f3065005bf226ff071301778
Breakerino <git@breakerino.me>
2025-08-21 23:54:16 +0200

    App: Add `Icon` component

--- packages/app/src/app/constants.ts
+++ packages/app/src/app/constants.ts
@@ -4,3 +4,5 @@
 export const DEFAULT_EVENT_BUS_NAMESPACE = 'breakerino';
 export const DEFAULT_EVENT_BUS_LABEL = 'Breakerino';
 export const DEFAULT_LOGGER_LABEL = 'Breakerino';
+
+export const ICON_SET_URI = '/assets/svg/icons.svg';
---
+++ packages/app/src/components/Icon.tsx
@@ -0,0 +1,33 @@
+// --------------------------------------------------------------------- 
+// Components > Icon
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import { ICON_SET_URI } from '@/app/constants';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+export interface IconProps {
+  id: string;
+  size?: number;
+  className?: string;
+}
+// --------------------------------------------------------------------- 
+
+const Icon: React.FC<IconProps> = ({ className, id }) => (
+  <svg
+    className={twMerge(clsx('brk-icon', 'w-auto h-6', className))}
+    aria-label={id}
+    role="img"
+  >
+    <use href={`${ICON_SET_URI}#${id}`} />
+  </svg>
+);
+
+export default Icon;
