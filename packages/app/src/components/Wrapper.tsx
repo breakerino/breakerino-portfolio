7a76cb24f6884497e4385e054e8fe8f4b5d69950
Breakerino <git@breakerino.me>
2025-08-21 23:54:51 +0200

    App: Add `Wrapper` component

---
+++ packages/app/src/components/Wrapper.tsx
@@ -0,0 +1,27 @@
+// --------------------------------------------------------------------- 
+// Components > Wrapper
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+export interface WrapperProps {
+	children: React.ReactNode;
+	className?: string;
+}
+// --------------------------------------------------------------------- 
+
+const Wrapper: React.FC<WrapperProps> = ({ className, children }) => {
+	return (
+		<div
+			className={twMerge(clsx('brk-wrapper', '@container/wrapper min-w-dvw min-h-dvh flex flex-col bg-secondary-950 text-primary-50', className))}>
+			{children}
+		</div>
+	);
+};
+
+export default Wrapper;
