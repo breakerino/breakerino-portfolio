b346e1562734e9a0a3f9968dfe8b69820d09811e
Breakerino <git@breakerino.me>
2025-08-21 23:46:09 +0200

    App: Move globals.scss into `assets/scss` folder

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -14,6 +14,7 @@ import Layout from '@/app/layout'
 // Stylesheets
 // --------------------------------------------------------------------- 
 import '@/assets/css/tailwind.css';
+import '@/assets/scss/globals.scss';
 // --------------------------------------------------------------------- 
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
