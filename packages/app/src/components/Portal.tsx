9b2cba470396f9ce5a1861661ab76c0ba8c74a50
Breakerino <git@breakerino.me>
2025-08-22 12:22:40 +0200

    App: Add `Portal` component

---
+++ packages/app/src/components/Portal.tsx
@@ -0,0 +1,31 @@
+// --------------------------------------------------------------------- 
+// Components > Portal
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+'use client';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import React from 'react';
+import { createPortal } from 'react-dom';
+import clsx from 'clsx';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+export interface PortalProps {
+	className?: string;
+	children: React.ReactNode;
+};
+// --------------------------------------------------------------------- 
+
+export default function Portal({ className, children }: PortalProps) {
+	if (typeof document === 'undefined') {
+		return null;
+	}
+	
+	return createPortal(
+		<div className={clsx('brk-portal', className)}>{children}</div>,
+		document.body
+	);
+}
