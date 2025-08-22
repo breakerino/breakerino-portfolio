4f9145ac956e2a938b415b21f70b98c9e09319fc
Breakerino <git@breakerino.me>
2025-08-22 19:17:05 +0200

    App: Index - Pass `socialItems` prop to `Header` component

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -6,7 +6,7 @@
 import Header from '@/components/Header';
 // --------------------------------------------------------------------- 
 
-import { HEADER_LOGO, HEADER_MENU_ITEMS } from '@/app/data';
+import { HEADER_LOGO, HEADER_MENU_ITEMS, SOCIAL_PROFILES } from '@/app/data';
 import Wrapper from '@/components/Wrapper';
 // --------------------------------------------------------------------- 
 
@@ -17,7 +17,7 @@ export type IndexProps = unknown
 export default function Index() {
 	return (
 		<Wrapper>
-			<Header logo={HEADER_LOGO} menuItems={HEADER_MENU_ITEMS} />
+			<Header logo={HEADER_LOGO} menuItems={HEADER_MENU_ITEMS} socialItems={SOCIAL_PROFILES} />
 		</Wrapper>
 	);
 }
