5541a5a6aaf9336569063136445f33f195d6b0c9
Breakerino <git@breakerino.me>
2025-08-22 19:14:39 +0200

    App: Types - Add `SocialSite` type

--- packages/app/src/app/types.ts
+++ packages/app/src/app/types.ts
@@ -1,6 +1,16 @@
+// --------------------------------------------------------------------- 
+// Types
+// --------------------------------------------------------------------- 
+
 export interface MenuItem {
   id: string;
   href: string;
   label: string;
   className?: string;
+}
+
+export interface SocialSite {
+	label: string;
+	icon: string;
+	baseURL: string;
 }
