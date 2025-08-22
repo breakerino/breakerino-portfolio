ef3870095b472c30ec8f024191ac6ed84441ca88
Breakerino <git@breakerino.me>
2025-08-22 13:23:13 +0200

    App: Add new hook `useMediaQuery`

---
+++ packages/app/src/hooks/useMediaQuery.ts
@@ -0,0 +1,30 @@
+// --------------------------------------------------------------------- 
+// Hooks > useMediaQuery
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+'use client'
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import { useState, useEffect } from 'react';
+// --------------------------------------------------------------------- 
+
+export function useMediaQuery(query: string, defaultValue = false) {
+	const [matches, setMatches] = useState(defaultValue);
+
+	useEffect(() => {
+		if (typeof window === 'undefined' || !window.matchMedia) return;
+
+		const mediaQuery = window.matchMedia(query);
+
+		setMatches(mediaQuery.matches);
+
+		const handler = (event: MediaQueryListEvent) => setMatches(event.matches);
+		mediaQuery.addEventListener('change', handler);
+
+		return () => mediaQuery.removeEventListener('change', handler);
+	}, [query]);
+
+	return [matches];
+}
