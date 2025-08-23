81b913e8f5d769c28cc2e6641c146439a57b0e57
Breakerino <git@breakerino.me>
2025-08-23 13:56:44 +0200

    App: Components/Container - Update component - format classes - define container query - add vertical flex layout - add default gaps

--- packages/app/src/components/Container.tsx
+++ packages/app/src/components/Container.tsx
@@ -10,14 +10,20 @@ import clsx from 'clsx';
 
 // --------------------------------------------------------------------- 
 export interface ContainerProps {
-	children: React.ReactNode;
+	children?: React.ReactNode;
 	className?: string;
 }
 // --------------------------------------------------------------------- 
 
 const Container: React.FC<ContainerProps> = ({ children, className }) => {
 	return (
-		<div className={twMerge(clsx('brk-container', 'w-full max-w-container px-6 md:px-8 lg:px-12 m-auto', className))}>
+		<div className={twMerge(
+			clsx(
+				'brk-container',
+				'@container/container w-full max-w-container flex flex-col gap-8 md:gap-12 lg:gap-18 px-6 md:px-8 lg:px-12 m-auto',
+				className
+			)
+		)}>
 			{children}
 		</div>
 	);
