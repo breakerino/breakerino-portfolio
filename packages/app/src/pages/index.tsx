51b750f543d485b508b6dded093f20b3551d6712
Breakerino <git@breakerino.me>
2025-08-23 14:03:02 +0200

    App: Pages/Index - Add content, hero and about me sections and footer

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -3,11 +3,16 @@
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-import Header from '@/components/Header';
-// --------------------------------------------------------------------- 
-
 import { HEADER_LOGO, HEADER_MENU_ITEMS, SOCIAL_PROFILES } from '@/app/data';
 import Wrapper from '@/components/Wrapper';
+import Content from '@/components/Content';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import Header from '@/sections/Header';
+import HeroSection from '@/sections/Hero';
+import AboutMeSection from '@/sections/AboutMe';
+import Footer from '@/sections/Footer';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
@@ -18,6 +23,11 @@ export default function Index() {
 	return (
 		<Wrapper>
 			<Header logo={HEADER_LOGO} menuItems={HEADER_MENU_ITEMS} socialItems={SOCIAL_PROFILES} />
+			<Content>
+				<HeroSection />
+				<AboutMeSection />
+			</Content>
+			<Footer />
 		</Wrapper>
 	);
 }
