eac9bdcfda05ca88b895733f57c74bdbdd1bdc81
Breakerino <git@breakerino.me>
2025-08-24 21:13:46 +0200

    Api: Settings/Site - Populate settings data and replace data placeholders

--- packages/api/src/api/settings/services/site.ts
+++ packages/api/src/api/settings/services/site.ts
@@ -4,26 +4,30 @@
 
 // --------------------------------------------------------------------- 
 import { DEFAULT_SITE_LOCALE } from '../../../utils/constants';
-import { sanitizeDocument, deepDocumentPopulate } from '../../../utils/functions';
+import { sanitizeDocument, deepDocumentPopulate, populateDocumentData } from '../../../utils/functions';
 // --------------------------------------------------------------------- 
 
 export default {
 	async getSettings() {
+		const globalSettings = await strapi.service('api::settings.settings').getSettings(['personal']);
 		
-		const settings = await strapi.documents('api::settings.site').findFirst({
+		let settings = (await strapi.documents('api::settings.site').findFirst({
 			populate: deepDocumentPopulate('api::settings.site'),
 			locale: DEFAULT_SITE_LOCALE,
 			status: 'published',
-		});
+		}) as Record<string, any>);
+		
+		settings = this.sanitizeSettings(settings);
+		settings = populateDocumentData(settings, globalSettings);
 
-		return this.sanitizeSettings(settings);
+		return settings;
 	},
 
 	sanitizeSettings(document: Record<string, unknown>) {
 		const settings = sanitizeDocument(document);
 		
 		return {
-			meta: sanitizeDocument(settings.siteMeta)
+			meta: sanitizeDocument(settings.seoMeta)
 		}
 	}
 };
