6022da1112271153dcd80fec068670c66fc6b570
Breakerino <git@breakerino.me>
2025-08-24 18:15:44 +0200

    Api: Extend `DOCUMENT_KEYS_TO_OMIT` with `IMAGE_FIELDS_TO_OMIT` and `SETTINGS_FIELDS_TO_OMIT`

--- packages/api/src/utils/constants.ts
+++ packages/api/src/utils/constants.ts
@@ -2,3 +2,36 @@
 // Utils > Constants
 // --------------------------------------------------------------------- 
 export const DEFAULT_SITE_LOCALE = 'en';
+
+export const SETTINGS_FIELDS_TO_OMIT = [ 
+	'settingsName',
+];
+
+export const IMAGE_FIELDS_TO_OMIT = [
+	'alternativeText',
+	'caption',
+	'formats',
+	'hash',
+	'ext',
+	'mime',
+	'size',
+	'previewUrl',
+	'provider',
+	'provider_metadata',
+	'folderPath',
+	'locale',
+];
+
+export const DOCUMENT_KEYS_TO_OMIT = [
+	'id',
+	'documentId',
+	'createdAt',
+	'updatedAt',
+	'createdBy',
+	'updatedBy',
+	'localizations',
+	'publishedAt',
+	'locale',
+	...IMAGE_FIELDS_TO_OMIT,
+	...SETTINGS_FIELDS_TO_OMIT,
+];
