d557473de85dd99a30c11e7770089b1017aa3add
Breakerino <git@breakerino.me>
2025-08-27 17:33:04 +0200

    App: Api - Add new endpoints

--- packages/app/src/app/api/endpoints.ts
+++ packages/app/src/app/api/endpoints.ts
@@ -7,12 +7,28 @@ import { EndpointsArgs } from './types';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-export type EndpointName = 'getData';
+export type EndpointName = 'getSiteSettings' | 'getPersonalSettings' | 'getLayoutSettings' | 'getSection' | 'getSections';
 // --------------------------------------------------------------------- 
 
 const endpoints: Record<EndpointName, EndpointsArgs> = {
-	getData: {
-		url: '/breakerino/data',
+	getSiteSettings: {
+		url: '/settings/site',
+		method: 'GET'
+	},
+	getPersonalSettings: {
+		url: '/settings/personal',
+		method: 'GET'
+	},
+	getLayoutSettings: {
+		url: '/settings/layout',
+		method: 'GET'
+	},
+	getSection: {
+		url: '/sections/:id',
+		method: 'GET'
+	},
+	getSections: {
+		url: '/sections',
 		method: 'GET'
 	},
 };
