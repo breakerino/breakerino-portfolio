77dd645c6146e3a2e3ae9e9863081b62fc6ff89e
Breakerino <git@breakerino.me>
2025-08-27 17:34:31 +0200

    App: MobileMenu - Change sidebar element to `aside`

--- packages/app/src/modules/MobileMenu/components/Sidebar.tsx
+++ packages/app/src/modules/MobileMenu/components/Sidebar.tsx
@@ -19,7 +19,7 @@ export interface SidebarProps {
 // --------------------------------------------------------------------- 
 const Sidebar: React.FC<SidebarProps> = ({ className, isOpened, children }) => {	
 	return (
-		<div className={twMerge(
+		<aside className={twMerge(
 			clsx(
 				'brk-menu brk-mobile-menu',
 				'@container/sidebar flex flex-col justify-between fixed top-0 left-0 z-20 w-[90dvw] max-w-[var(--spacing-menu)] h-dvh  bg-secondary-950 border-r-3 border-secondary-700 rounded-xl rounded-r-none transition-transform duration-300 ease-in-out -translate-x-full',
@@ -28,7 +28,7 @@ const Sidebar: React.FC<SidebarProps> = ({ className, isOpened, children }) => {
 			)
 		)}>
 			{children}
-		</div>
+		</aside>
 	)
 }
 
