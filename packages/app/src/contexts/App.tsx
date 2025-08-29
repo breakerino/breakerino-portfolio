9c18396f8fb9a28f5b1514533bf9fccc1ac0aab1
Breakerino <git@breakerino.me>
2025-08-29 12:56:19 +0200

    App: Add `App` context

---
+++ packages/app/src/contexts/App.tsx
@@ -0,0 +1,84 @@
+// --------------------------------------------------------------------- 
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import { HeaderProps } from '@/containers/Header';
+import { FooterProps } from '@/containers/Footer';
+import { ContentProps } from '@/containers/Content';
+import { PersonalSettings, SiteSettings } from '@/app/types';
+// --------------------------------------------------------------------- 
+
+export interface AppContextProps {
+	children?: React.ReactNode | React.ReactNode[];
+}
+
+export interface AppContextStore {
+	settings: {
+		personal: PersonalSettings;
+		site: SiteSettings;
+	};
+	sections: {
+		header: HeaderProps['data'];
+		content: ContentProps['data'];
+		footer: FooterProps['data'];
+	};
+}
+
+const AppContext = React.createContext<AppContextStore | undefined>(undefined);
+
+export const AppContextProvider: React.FC<AppContextProps> = ({ children }) => {
+	const { data: siteSettings } = useQuery<SiteSettings>({
+		queryKey: ['settings', 'site'],
+		suspense: true,
+	});
+	
+	const { data: personalSettings } = useQuery<PersonalSettings>({
+		queryKey: ['settings', 'personal'],
+		suspense: true,
+	});
+	
+	const { data: headerData } = useQuery<AppContextStore['sections']['header']>({
+		queryKey: ['sections', 'header'],
+		suspense: true,
+	});
+	
+	const { data: contentData } = useQuery<AppContextStore['sections']['content']>({
+		queryKey: ['sections'],
+		suspense: true,
+	});
+	
+	const { data: footerData } = useQuery<AppContextStore['sections']['footer']>({
+		queryKey: ['sections', 'footer'],
+		suspense: true,
+	});
+
+	return (
+		<AppContext.Provider
+			value={{
+				settings: {
+					site: siteSettings!,
+					personal: personalSettings!
+				},
+				sections: {
+					header: headerData!,
+					content: contentData!,
+					footer: footerData!,
+				},
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+export const useAppContext = () => {
+	const context = React.useContext(AppContext);
+
+	if (!context) {
+		throw new Error('useAppContext must be used within an AppContextProvider');
+	}
+
+	return context;
+};
