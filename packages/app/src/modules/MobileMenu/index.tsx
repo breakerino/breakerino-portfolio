358ea6f6871c25d0d2b0363fc74421e380ad1c43
Breakerino <git@breakerino.me>
2025-08-29 13:08:23 +0200

    App: Update `MobileMenu` module - remove `breakpoint` prop - ensure is properly hidden on desktop

--- packages/app/src/modules/MobileMenu/index.tsx
+++ packages/app/src/modules/MobileMenu/index.tsx
@@ -1,43 +1,44 @@
 // --------------------------------------------------------------------- 
-// Components > MobileMenu
+// Components > Mobile menu
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-import React from 'react';
-import clsx from 'clsx';
+import React from 'react'
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
 // --------------------------------------------------------------------- 
 
-// --------------------------------------------------------------------- 
-import { MenuItem, SocialProfile } from '@/app/types';
-import { LogoProps } from '@/components/Logo';
-import SocialLink from '@/components/SocialLink';
-import { useMediaQuery } from '@/hooks/useMediaQuery';
-// --------------------------------------------------------------------- 
-
-// --------------------------------------------------------------------- 
-import Hamburger from './components/Hamburger';
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import Overlay from './components/Overlay';
+import { BaseComponentProps, MenuItem } from '@/app/types'
+import { LogoProps } from '@/components/Logo'
+import SocialLink from '@/components/SocialLink'
+import { SocialProfile } from '@/app/types'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 // --------------------------------------------------------------------- 
 
+import Hamburger from './components/Hamburger'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Navigation from './components/Navigation'
+import Footer from './components/Footer'
+import Overlay from './components/Overlay'
 // --------------------------------------------------------------------- 
-export interface MobileMenuProps {
-	className?: string;
-	logo: Omit<LogoProps, 'className' | 'href'>;
-	menuItems: MenuItem[];
-	socialItems?: SocialProfile[];
-	ariaLabel?: string;
-	breakpoint?: number;
+
+export interface MobileMenuProps extends BaseComponentProps {
+	logo: Omit<LogoProps, 'className' | 'href'>
+	menuItems: MenuItem[]
+	socialItems?: SocialProfile[]
+	ariaLabel?: string
 }
-// --------------------------------------------------------------------- 
 
-// --------------------------------------------------------------------- 
-const MobileMenu: React.FC<MobileMenuProps> = ({ className, logo, menuItems, socialItems, breakpoint, ariaLabel: navigationAriaLabel }) => {
-	const [isOpened, setIsOpened] = React.useState<boolean>(false);
-	const [isMobile] = useMediaQuery(`(max-width: ${breakpoint ? `${breakpoint}px` : '767px'})`)
+const MobileMenu: React.FC<MobileMenuProps> = ({
+	className,
+	logo,
+	menuItems,
+	socialItems,
+	ariaLabel: navigationAriaLabel,
+}) => {
+	const [isOpened, setIsOpened] = React.useState<boolean>(false)
+	const [isMobile] = useMediaQuery('(max-width: 47.9375rem)')
 
 	const handleToggle = () => setIsOpened(state => !state);
 	const handleClose = () => setIsOpened(false);
@@ -52,26 +53,42 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ className, logo, menuItems, soc
 
 	return (
 		<>
-			<Hamburger className={className} onClick={handleToggle} />
+			<Hamburger
+				className={twMerge(
+					className,
+					'md:hidden'
+				)}
+				onClick={handleToggle}
+			/>
 
-			<Sidebar isOpened={isOpened}>
+			<Sidebar className="md:hidden" isOpened={isOpened} >
 				<Header className="pl-10 pr-5 py-5" logo={logo} onClose={handleClose} />
-				<Navigation className="px-10 py-8 flex-1" items={menuItems} ariaLabel={navigationAriaLabel} onClose={handleClose} />
-				<Footer className="pl-10 pr-8 py-5">
+				<Navigation
+					className="px-10 py-8 flex-1"
+					items={menuItems}
+					ariaLabel={navigationAriaLabel}
+					onClose={handleClose}
+				/>
+				<Footer className="pl-10 pr-8 py-5" >
 					{socialItems && (
-						<div className={clsx(
-							'brk-mobile-menu-socials',
-							'w-full flex flex-wrap gap-2'
-						)}>
-							{socialItems.map((item) => <SocialLink key={item.id} variant="xs" showLabel={false} {...item} />)}
+						<div className={clsx('brk-mobile-menu-socials w-full flex flex-wrap gap-2')}>
+							{
+								socialItems.map(item => (
+									<SocialLink key={item.type} variant="xs" showLabel={false} {...item} />
+								))
+							}
 						</div>
 					)}
 				</Footer>
 			</Sidebar>
 
-			<Overlay isOpened={isOpened} onClose={handleClose} />
+			<Overlay
+				className="md:hidden"
+				isOpened={isOpened}
+				onClose={handleClose}
+			/>
 		</>
 	)
 }
 
-export default MobileMenu;
+export default MobileMenu
