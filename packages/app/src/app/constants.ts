60be9859da0e8d5730daf82ac5bd384da93ae7b0
Breakerino <git@breakerino.me>
2025-08-29 12:55:04 +0200

    App: Add/update constants

--- packages/app/src/app/constants.ts
+++ packages/app/src/app/constants.ts
@@ -4,16 +4,23 @@
 
 // --------------------------------------------------------------------- 
 import { SocialSite } from '@/app/types';
+import HeroSection from '@/sections/Hero';
+import AboutMeSection from '@/sections/AboutMe';
+import SkillsSection from '@/sections/Skills';
+import ExperienceSection from '@/sections/Experience';
+import ProjectsSection from '@/sections/Projects';
+import ContactSection from '@/sections/Contact';
 // --------------------------------------------------------------------- 
 
+// --------------------------------------------------------------------- 
+// General
+// --------------------------------------------------------------------- 
 export const DEFAULT_EVENT_BUS_NAMESPACE = 'breakerino';
 export const DEFAULT_EVENT_BUS_LABEL = 'Breakerino';
 export const DEFAULT_LOGGER_LABEL = 'Breakerino';
 
-export const ICON_SET_URI = '/assets/svg/icons.svg';
-
 // --------------------------------------------------------------------- 
-// Socials
+// Social sites
 // --------------------------------------------------------------------- 
 export const SOCIAL_SITES: Record<string, SocialSite> = {
 	github: {
@@ -52,3 +59,26 @@ export const SOCIAL_SITES: Record<string, SocialSite> = {
 		baseURL: 'https://myanimelist.net/profile'
 	}
 }
+
+// --------------------------------------------------------------------- 
+// Icons
+// --------------------------------------------------------------------- 
+export const ICON_SET_URI = '/assets/svg/icons.svg';
+
+// --------------------------------------------------------------------- 
+// Sections
+// --------------------------------------------------------------------- 
+export const SECTION_COMPONENT_MAP = {
+	'hero': HeroSection,
+	'about-me': AboutMeSection,
+	'skills': SkillsSection,
+	'projects': ProjectsSection,
+	'experience': ExperienceSection,
+	'contact': ContactSection
+}
+
+// --------------------------------------------------------------------- 
+// React Query
+// --------------------------------------------------------------------- 
+export const DEFAULT_STALE_TIME = 60 * 60 * 1000; // 1h
+export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 1000; // 24h
