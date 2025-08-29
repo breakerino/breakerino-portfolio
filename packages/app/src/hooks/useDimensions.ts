521475aea5f010b513090aafdf2ad8b304bfe4f0
Breakerino <git@breakerino.me>
2025-08-29 15:58:54 +0200

    App: Add hook `useDimensions`

---
+++ packages/app/src/hooks/useDimensions.ts
@@ -0,0 +1,43 @@
+// --------------------------------------------------------------------- 
+// Hooks > useDimensions
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import React from 'react';
+// --------------------------------------------------------------------- 
+
+export interface Dimensions {
+	width: number;
+	height: number;
+}
+
+export function useDimensions<T extends HTMLElement>(ref: React.RefObject<T | null>) {
+	const [dimensions, setDimensions] = React.useState<Dimensions>({
+		width: 0,
+		height: 0
+	});
+
+	React.useLayoutEffect(() => {
+		const node = ref.current;
+
+		if (!node) {
+			return;
+		}
+
+		const measureDimensions = () => {
+			const { width, height } = node.getBoundingClientRect();
+			setDimensions({ width, height });
+		};
+
+		measureDimensions();
+
+		const resizeObserver = new ResizeObserver(measureDimensions);
+		resizeObserver.observe(node);
+
+		return () => {
+			resizeObserver.disconnect();
+		};
+	}, [ref]);
+
+	return dimensions;
+}
