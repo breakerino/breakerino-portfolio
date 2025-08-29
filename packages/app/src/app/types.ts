9a43fae26f9fb30bca8223f52cfca4efb3344c22
Breakerino <git@breakerino.me>
2025-08-29 12:54:58 +0200

    App: Add/update types

--- packages/app/src/app/types.ts
+++ packages/app/src/app/types.ts
@@ -1,17 +1,54 @@
 // --------------------------------------------------------------------- 
-// Types
+// App > Types
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-import { SOCIAL_SITES } from '@/app/constants';
+import { SECTION_COMPONENT_MAP, SOCIAL_SITES } from '@/app/constants';
 // --------------------------------------------------------------------- 
 
-export interface MenuItem {
-  id: string;
-  href: string;
-  label: string;
-  className?: string;
+// --------------------------------------------------------------------- 
+// Base
+// ---------------------------------------------------------------------
+export interface BaseComponentProps {
+	className?: string;
+	children?: React.ReactNode;
+	as?: keyof React.JSX.IntrinsicElements;
 }
+
+export interface BaseSectionProps extends BaseComponentProps {
+	id: string;
+	heading?: Heading;
+}
+
+// --------------------------------------------------------------------- 
+// General
+// ---------------------------------------------------------------------
+export interface MenuItem {
+	id: string;
+	href: string;
+	label: string;
+	className?: string;
+}
+
+export interface Heading {
+	title: string;
+	subtitle?: string;
+}
+
+export interface NavigationItem {
+	label: string;
+	slug: string;
+}
+
+export interface Image {
+	alternativeText: string;
+	name: string;
+	url: string;
+	width: number;
+	height: number;
+}
+
+export type Video = Record<string, unknown>;
 
 export interface SocialSite {
 	label: string;
@@ -19,7 +56,49 @@ export interface SocialSite {
 	baseURL: string;
 }
 
+export type SocialSiteType = keyof typeof SOCIAL_SITES;
+
 export interface SocialProfile {
-	id: keyof typeof SOCIAL_SITES;
+	type: SocialSiteType;
 	username: string;
 }
+
+// --------------------------------------------------------------------- 
+// Settings
+// --------------------------------------------------------------------- 
+
+export interface GeneralSettings {
+	year: number;
+}
+
+export interface SiteSettings {
+	meta: {
+		title: string;
+		description: string;
+		keywords: string;
+	}
+}
+
+export interface PersonalSettings {
+	age: number;
+	yearsOfExperience: number;
+	location: string;
+	socials: SocialProfile[];
+};
+
+export interface LayoutSettings {
+	sections: {
+		id: string;
+		component: string;
+		heading?: {
+			title: string;
+			subtitle?: string;
+		}
+	}[]
+}
+
+// --------------------------------------------------------------------- 
+// Section
+// --------------------------------------------------------------------- 
+export type SectionData = Record<string, unknown>;
+export type SectionType = keyof typeof SECTION_COMPONENT_MAP;
