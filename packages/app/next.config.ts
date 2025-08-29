901199ed6dde8a746d9bcf79239a18d1cdb6b3c8
Breakerino <git@breakerino.me>
2025-08-29 12:53:36 +0200

    App: Config - Add static server URL to remote patterns to allow to serve assets from external sources

--- packages/app/next.config.ts
+++ packages/app/next.config.ts
@@ -2,9 +2,29 @@
 import type { NextConfig } from 'next';
 // --------------------------------------------------------------------- 
 
+const remoteURLs = [
+	process.env?.NEXT_PUBLIC_STATIC_BASE_URL,
+].filter(Boolean) as string[];
+
+const remotePatterns = remoteURLs.map((url) => {
+	const parsedURL = new URL(url);
+	
+	return {
+		protocol: parsedURL.protocol.replace(':', '') as 'http' | 'https',
+		hostname: parsedURL.hostname,
+		...(parsedURL.port ? { port: parsedURL.port } : {}),
+		pathname: '/**',
+	};
+});
+
 const nextConfig: NextConfig = {
-  output: 'standalone',
-	devIndicators: false
+	output: 'standalone',
+	devIndicators: false,
+	basePath: '',
+	assetPrefix: '',
+	images: {
+		remotePatterns,
+	},
 };
 
 export default nextConfig;
