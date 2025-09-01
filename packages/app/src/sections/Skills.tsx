f590541c45aac692d28d5a2ee56b5f2080f82624
Breakerino <git@breakerino.me>
2025-09-01 15:59:29 +0200

    App: Update `Skills` section - update prop types - responsive skills listing

--- packages/app/src/sections/Skills.tsx
+++ packages/app/src/sections/Skills.tsx
@@ -9,17 +9,26 @@ import { twMerge } from 'tailwind-merge';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-import { BaseSectionProps } from '../app/types';
+import { BaseSectionProps, Skill } from '@/app/types';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
 import Section from '@/components/Section';
 import Container from '@/components/Container';
 import Heading from '@/components/Heading';
+import SkillsList from '@/components/SkillsList';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-export interface SkillsSectionProps extends BaseSectionProps {}
+export interface SkillsSectionProps extends BaseSectionProps {
+	skills: {
+		title: string;
+		skills: Skill[]
+	}[]
+}
 // --------------------------------------------------------------------- 
 
-const SkillsSection: React.FC<SkillsSectionProps> = ({ id, className, heading }) => {
+const SkillsSection: React.FC<SkillsSectionProps> = ({ id, className, heading, skills }) => {
+	const [isMobile] = useMediaQuery('(max-width: 767px)');
+
 	return (
 		<Section
 			id={id}
@@ -31,6 +40,21 @@ const SkillsSection: React.FC<SkillsSectionProps> = ({ id, className, heading })
 			)}>
 			<Container>
 				{heading && <Heading {...heading} />}
+				<div className="flex flex-col gap-12">
+					{skills.map(({ title, skills }) => (
+						<div
+							key={title}
+							className="flex flex-col gap-2 md:gap-3 lg:gap-4"
+						>
+							<h3
+								className="font-bold text-2xl md:text-4xl lg:text-[2.5rem] text-primary-100"
+							>
+								{title}
+							</h3>
+							<SkillsList size={isMobile ? 'small' : 'medium'} {...{ skills }} />
+						</div>
+					))}
+				</div>
 			</Container>
 		</Section>
 	)
