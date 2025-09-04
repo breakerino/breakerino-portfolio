2ba0ac658a0e81adc738dba0363d2593726d688b
Breakerino <git@breakerino.me>
2025-09-04 13:28:33 +0200

    App: Update `Hero` section

--- packages/app/src/sections/Hero.tsx
+++ packages/app/src/sections/Hero.tsx
@@ -3,9 +3,8 @@
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
-import clsx from 'clsx';
-import Image from 'next/image';
 import React from 'react';
+import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { Image as ImageType, SocialSiteType } from '@/app/types';
 // --------------------------------------------------------------------- 
@@ -16,10 +15,11 @@ import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { BaseSectionProps } from '@/app/types';
 import Container from '@/components/Container';
 import Section from '@/components/Section';
-import SocialLink from '@/components/SocialLink';
+import Link from '@/components/Link';
 import Text from '@/components/Text';
 import { getStaticAssetURL } from '@/app/functions';
 import PhotoFrame from '@/components/PhotoFrame';
+import { SOCIAL_SITES } from '@/app/constants';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
@@ -34,8 +34,8 @@ export interface HeroSectionProps extends BaseSectionProps {
 const HeroSection: React.FC<HeroSectionProps> = ({ className, id, heading, subheading, text, socials, image }) => {
 	const { settings } = useAppContext();
 
-	const [isTablet] = useMediaQuery('(max-width: 1023px)');
-	const [isMobile] = useMediaQuery('(max-width: 767px)');
+	const [isTablet] = useMediaQuery('(max-width: 64rem)');
+	const [isMobile] = useMediaQuery('(max-width: 48rem)');
 
 	const socialProfiles = React.useMemo(() => {
 		return settings.personal.socials.filter(social => socials?.includes(social.type))
@@ -70,10 +70,16 @@ const HeroSection: React.FC<HeroSectionProps> = ({ className, id, heading, subhe
 								</span>
 							</h1>
 						</div>
-						<Text>{text}</Text>
+						<Text withMarkdown>{text}</Text>
 						<div className="flex gap-4">
-							{socialProfiles.map((profile) => (
-								<SocialLink key={profile.type} variant={isMobile ? 'sm' : isTablet ? 'md' : 'lg'} {...profile} />
+							{socialProfiles.map(({type, username}) => (
+								<Link 
+									key={type}
+									size={isMobile ? 'sm' : isTablet ? 'md' : 'lg'}  
+									icon={SOCIAL_SITES[type].icon}
+									text={SOCIAL_SITES[type].label}
+									url={`${SOCIAL_SITES[type].baseURL}/${username}`}
+								/>
 							))}
 						</div>
 					</div>
