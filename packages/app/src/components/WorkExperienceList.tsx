41c233ce7ad22dc75f02759c89523c0030b644be
Breakerino <git@breakerino.me>
2025-09-06 19:37:28 +0200

    App: Add `<WorkExperienceList>` component

---
+++ packages/app/src/components/WorkExperienceList.tsx
@@ -0,0 +1,42 @@
+// --------------------------------------------------------------------- 
+// Components > Work experience list
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import { BaseComponentProps, WorkExperience } from '@/app/types';
+import WorkExperienceItem from './WorkExperienceItem';
+// --------------------------------------------------------------------- 
+
+export interface WorkExperienceListProps extends Omit<BaseComponentProps, 'children'> {
+	items: WorkExperience[]
+}
+
+const WorkExperienceList: React.FC<WorkExperienceListProps> = ({ className, as: Tag = 'ul', items }) => {
+	return (
+		<Tag
+			className={twMerge(
+				clsx(
+					'brk-work-experience-list',
+					'flex flex-col gap-4',
+					className
+				)
+			)}
+		>
+			{items.map((item) => (
+				<WorkExperienceItem
+					key={item.id}
+					as="li"
+					{...item}
+				/>
+			))}
+		</Tag>
+	)
+}
+
+export default WorkExperienceList;
