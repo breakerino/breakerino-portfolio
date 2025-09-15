395bdda6c2704b1e0e01ae0a16de4cac2f2e2770
Breakerino <git@breakerino.me>
2025-09-15 19:46:29 +0000

    App: Link - Add variant styles (default, animated)

--- packages/app/src/components/Link.tsx
+++ packages/app/src/components/Link.tsx
@@ -17,6 +17,7 @@ import { BaseComponentProps } from '@/app/types';
 
 export interface LinkProps extends BaseComponentProps, LinkType {
 	size?: 'xs' | 'sm' | 'md' | 'lg';
+	variant?: 'default' | 'animated';
 	showLabel?: boolean;
 	target?: HTMLAttributeAnchorTarget;
 }
@@ -29,15 +30,22 @@ const Link: React.FC<LinkProps> = ({
 	showLabel = true,
 	target = '_self',
 	size = 'md',
+	variant = 'default'
 }) => {
 	const sizes: Record<NonNullable<LinkProps['size']>, { wrapper: string, icon: string; text: string }> = {
-		xs: { icon: 'w-6 h-6', text: 'text-base', wrapper: 'gap-1.5 p-1.5' },
-		sm: { icon: 'w-8 h-8', text: 'text-lg', wrapper: 'gap-2 p-2' },
-		md: { icon: 'w-10 h-10', text: 'text-xl', wrapper: twMerge(clsx('gap-2.5 px-3 py-2.5', !showLabel && 'p-2.5')) },
-		lg: { icon: 'w-12 h-12', text: 'text-2xl', wrapper: twMerge(clsx('gap-3 px-4 py-2.5', !showLabel && 'p-2.5')) },
+		xs: { wrapper: 'gap-1.5 p-1.5', icon: 'w-6 h-6', text: 'text-base' },
+		sm: { wrapper: 'gap-2 p-2', icon: 'w-8 h-8', text: 'text-lg' },
+		md: { wrapper: twMerge(clsx('gap-2.5 px-3 py-2.5', !showLabel && 'p-2.5')), icon: 'w-10 h-10', text: 'text-xl' },
+		lg: { wrapper: twMerge(clsx('gap-3 px-4 py-2.5', !showLabel && 'p-2.5')), icon: 'w-12 h-12', text: 'text-2xl' },
 	};
 
+	const variants: Record<NonNullable<LinkProps['variant']>, { wrapper: string }> = {
+		default: { wrapper: '' },
+		animated: { wrapper: 'hover:-translate-y-1 transition-transform duration-300 ease-in-out' }
+	}
+
 	const sizeStyles = sizes[size];
+	const variantStyles = variants[variant];
 
 	return (
 		<NextLink
@@ -48,6 +56,7 @@ const Link: React.FC<LinkProps> = ({
 					'brk-link',
 					'w-fit flex items-center border-2 border-secondary-700 rounded-lg hover:bg-secondary-800 active:bg-secondary-900 transition-colors',
 					sizeStyles.wrapper,
+					variantStyles.wrapper,
 					className
 				)
 			)}
