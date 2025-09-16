c1ada4e26eeb455641c2711cddb207db0ce3cedf
Breakerino <git@breakerino.me>
2025-09-16 00:53:15 +0000

    App: DesktopMenu - Extend functionality - add active menu item indicator - set navigation state on click

--- packages/app/src/modules/DesktopMenu/index.tsx
+++ packages/app/src/modules/DesktopMenu/index.tsx
@@ -11,39 +11,78 @@ import clsx from 'clsx';
 
 // ---------------------------------------------------------------------
 import { BaseComponentProps, MenuItem } from '@/app/types';
+import { useAppContext } from '@/contexts/App';
 // ---------------------------------------------------------------------
 
 export interface DesktopMenuProps extends BaseComponentProps {
 	menuItems: MenuItem[];
+	activeMenuItemID?: string | null;
 	ariaLabel?: string;
 }
 
-const DesktopMenu: React.FC<DesktopMenuProps> = ({ menuItems, className, ariaLabel }) => {
+const DesktopMenu: React.FC<DesktopMenuProps> = ({ menuItems, activeMenuItemID, className, ariaLabel }) => {
+	const { actions } = useAppContext();
+	
+	const activeItemRef = React.useRef<HTMLLIElement>(null);
+
+	React.useEffect(() => {
+		if (!(activeItemRef.current instanceof HTMLLIElement)) {
+			document.documentElement.style.setProperty('--brk-menu-indicator-width', '0px');
+			document.documentElement.style.setProperty('--brk-menu-indicator-offset-x', '0px');
+			return () => void 1;
+		}
+
+		const activeItemRect = activeItemRef.current.getBoundingClientRect();
+
+		document.documentElement.style.setProperty('--brk-menu-indicator-width', `${activeItemRect.width}px`);
+		document.documentElement.style.setProperty('--brk-menu-indicator-offset-x', `${activeItemRef.current.offsetLeft}px`);
+		return () => void 1;
+	}, [activeMenuItemID]);
+	
+	const handleNavigation = () => {
+		actions.setIsNavigating(true);
+		
+		setTimeout(() => {
+			actions.setIsNavigating(false);
+		}, 1000)
+	}
+
 	return (
 		<nav
 			className={twMerge(
 				clsx(
 					'brk-menu brk-desktop-menu',
-					'flex items-center',
+					'relative flex items-center',
 					className)
 			)}
 			aria-label={ariaLabel ?? 'Desktop navigation'}
 		>
 			<ul className="brk-menu-list flex items-center gap-8">
 				{menuItems.map(({ id, href, label, className }) => (
-					<li className="brk-menu-list__item" key={id}>
+					<li className="brk-menu-list__item" key={id} ref={id === activeMenuItemID ? activeItemRef : null}>
 						<Link
 							href={href}
 							className={clsx(
-								'brk-menu-list__link text-lg lg:text-xl no-underline hover:underline transition-colors',
+								'brk-menu-list__link text-lg lg:text-xl no-underline transition-colors',
 								className
 							)}
+							onClick={handleNavigation}
 						>
 							{label}
 						</Link>
 					</li>
 				))}
 			</ul>
+
+			<div
+				className={clsx(
+					'brk-menu-item-indicator',
+					'absolute -bottom-1.5 bg-primary-400 rounded-sm',
+					'transition-all duration-300 ease-in-out',
+					'left-[var(--brk-menu-indicator-offset-x)]',
+					'w-[var(--brk-menu-indicator-width)] h-1'
+				)}
+			/>
 		</nav>
 	);
 };
