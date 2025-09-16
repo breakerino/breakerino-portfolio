d47441db13255bedff6f3051c49024463140152d
Breakerino <git@breakerino.me>
2025-09-16 00:52:16 +0000

    App: Constants - Add `TRANSITION_DURATION` constant

--- packages/app/src/app/constants.ts
+++ packages/app/src/app/constants.ts
@@ -19,6 +19,8 @@ export const DEFAULT_EVENT_BUS_NAMESPACE = 'breakerino';
 export const DEFAULT_EVENT_BUS_LABEL = 'Breakerino';
 export const DEFAULT_LOGGER_LABEL = 'Breakerino';
 
+export const TRANSITION_DURATION = 300;
+
 // --------------------------------------------------------------------- 
 // Social sites
 // --------------------------------------------------------------------- 
