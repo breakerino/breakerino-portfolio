260ed35e9cf441213b572b9fbc56e2988df5a7fb
Breakerino <git@breakerino.me>
2025-09-16 00:51:10 +0000

    App: Portal - Fix SSR errors

--- packages/app/src/components/Portal.tsx
+++ packages/app/src/components/Portal.tsx
@@ -18,19 +18,25 @@ import { BaseComponentProps } from '@/app/types';
 
 export interface PortalProps extends BaseComponentProps {}
 
-const Portal: React.FC<PortalProps> = ({ className, children }) => {	
-	const portalRoot = React.useMemo(() => {
-		return document.querySelector('.brk-portal-root');
-	}, []);
-	
-	if (typeof document === 'undefined' || ! ( portalRoot instanceof HTMLElement )) {
-		return null;
-	}
-	
-	return createPortal(
-		<div className={clsx('brk-portal', className)}>{children}</div>,
-		portalRoot
-	);
-}
+const Portal: React.FC<PortalProps> = ({ className, children }) => {
+  const [portalRoot, setPortalRoot] = React.useState<HTMLElement | null>(null);
+
+  React.useEffect(() => {
+    const root = document.querySelector('.brk-portal-root');
+		
+    if (root instanceof HTMLElement) {
+      setPortalRoot(root);
+    }
+  }, []);
+
+  if (!portalRoot) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={clsx('brk-portal', className)}>{children}</div>,
+    portalRoot
+  );
+};
 
 export default Portal;
