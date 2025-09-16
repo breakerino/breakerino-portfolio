63bb9adf2f38fff1fad7c012311808b6b882142a
Breakerino <git@breakerino.me>
2025-09-16 00:50:00 +0000

    App: useHeader - Add scroll debounce and toggle off visiblity on navigation

--- packages/app/src/hooks/useHeader.ts
+++ packages/app/src/hooks/useHeader.ts
@@ -3,41 +3,57 @@
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
+import { debounce } from 'lodash';
 import React from 'react';
+// --------------------------------------------------------------------- 
+
+// --------------------------------------------------------------------- 
+import { TRANSITION_DURATION } from '@/app/constants';
+import { useAppContext } from '@/contexts/App';
 // --------------------------------------------------------------------- 
 
 export interface UseHeaderProps {
 	stickyThreshold: number;
 	showOffset: number;
 	hideOffset: number;
+	delay?: number;
 }
 
-const useHeader = ({stickyThreshold, showOffset, hideOffset}: UseHeaderProps) => {
+const useHeader = ({stickyThreshold, showOffset, hideOffset, delay = 50}: UseHeaderProps) => {
+	const { store: {isNavigating} } = useAppContext();
+	
 	const [isVisible, setIsVisible] = React.useState<boolean>(false);
 	const [isSticky, setIsSticky] = React.useState<boolean>(false);
 	const [isAnimated, setIsAnimated] = React.useState<boolean>(false);
 	
 	const lastScrollY = React.useRef(0);
 	
-	const handleScroll = () => {
+	const handleScroll = debounce(() => {
     const currentScrollY = window.scrollY;
 
 		// Sticky
 		setIsSticky(currentScrollY > stickyThreshold);
 		
+		// Animated
 		setTimeout(() => {
 			setIsAnimated(currentScrollY > stickyThreshold)
-		}, 300);
+		}, TRANSITION_DURATION);
 
 		// Visibility
-    if (currentScrollY < (lastScrollY.current - showOffset)) {
+    if (!isNavigating && currentScrollY < (lastScrollY.current - showOffset)) {
       setIsVisible(true);
     } else if (currentScrollY > (lastScrollY.current + hideOffset)) {
       setIsVisible(false);
     }
 
     lastScrollY.current = currentScrollY;
-  };
+  }, delay);
+	
+	React.useEffect(() => {
+		if ( isNavigating ) {
+			setIsVisible(false);
+		}
+	}, [isNavigating, isVisible]);
 
   React.useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -47,11 +63,7 @@ const useHeader = ({stickyThreshold, showOffset, hideOffset}: UseHeaderProps) =>
     };
   }, []);
 	
-	return {
-		isSticky,
-		isVisible,
-		isAnimated
-	}
+	return { isSticky, isVisible, isAnimated }
 }
 
 export default useHeader;
