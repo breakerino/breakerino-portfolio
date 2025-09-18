2a8ec1f8a9eed35d7e26a81c660db1bb340cee35
Breakerino <git@breakerino.me>
2025-09-18 23:11:56 +0000

    App: About me - Implement `<Reels>` component

--- packages/app/src/sections/AboutMe.tsx
+++ packages/app/src/sections/AboutMe.tsx
@@ -4,10 +4,9 @@
 
 // --------------------------------------------------------------------- 
 import React from 'react';
-import Image from 'next/image';
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { Image as ImageType, Video as VideoType } from '@/app/types';
+import { Media } from '@/app/types';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
@@ -16,28 +15,19 @@ import Container from '@/components/Container';
 import Text from '@/components/Text';
 import Heading from '@/components/Heading';
 import { BaseSectionProps } from '../app/types';
-import { getStaticAssetURL } from '@/app/functions';
 import Frame from '@/components/Frame';
 import Motion from '@/modules/motion';
+import Reels from '@/components/Reels';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
 export interface AboutMeSectionProps extends BaseSectionProps {
 	text: string;
-	reels: {
-		title: string;
-		video: VideoType
-		thumbnail?: {
-			image: ImageType
-		}
-	}[]
+	reels: Media[];
 }
 // --------------------------------------------------------------------- 
 
 const AboutMeSection: React.FC<AboutMeSectionProps> = ({ className, heading, reels, text }) => {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const reel = reels[0] as any;
-
 	return (
 		<Section
 			id="about-me"
@@ -50,6 +40,12 @@ const AboutMeSection: React.FC<AboutMeSectionProps> = ({ className, heading, ree
 			<Container>
 				{heading && <Heading {...heading} />}
 				<div className="flex flex-col md:flex-row gap-8">
+					<Frame className="md:sticky md:top-8" animated>
+						<Reels
+							className="2xs:w-[33.33333vw] 2xs:min-w-[18.75rem] md:min-w-[unset] lg:w-[27.5vw]"
+							reels={reels}
+						/>
+					</Frame>
 					<div className="flex flex-1 md:mt-8 lg:mt-12 items-center">
 						<Motion.ScrollReveal
 							initial={{ opacity: 0, y: 32 }}
