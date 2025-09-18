15cc4db238265a2777f1cbba329592be99168c02
Breakerino <git@breakerino.me>
2025-09-18 23:15:44 +0000

    App: Root - Implement `<Cursor>` component and buttery smooth scrolling via `lenis`

--- packages/app/src/layouts/Root.tsx
+++ packages/app/src/layouts/Root.tsx
@@ -11,6 +11,10 @@ import { Inter } from 'next/font/google';
 import Head from '@/components/Head';
 import SkipLink from '@/components/SkipLink';
 import PortalRoot from '@/components/PortalRoot';
+import Cursor from '@/components/Cursor';
+import ReactLenis from 'lenis/react';
+import type { LenisRef } from 'lenis/react';
+import { cancelFrame, frame } from 'framer-motion';
 // --------------------------------------------------------------------- 
 
 // --------------------------------------------------------------------- 
@@ -23,15 +27,32 @@ export interface RootLayoutProps {
 }
 // --------------------------------------------------------------------- 
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
-	<html lang="en" className={`${interFont.variable}`}>
-		<Head />
-		<body className="bg-secondary-950 text-primary-50">
-			<SkipLink />
-			{children}
-			<PortalRoot />
-		</body>
-	</html>
-)
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+	const lenisRef = React.useRef<LenisRef>(null)
+
+	React.useEffect(() => {
+		function update(data: { timestamp: number }) {
+			const time = data.timestamp
+			lenisRef.current?.lenis?.raf(time)
+		}
+
+		frame.update(update, true)
+
+		return () => cancelFrame(update)
+	}, [])
+
+	return (
+		<html lang="en" className={`${interFont.variable}`}>
+			<Head />
+			<body className="bg-secondary-950 text-primary-50">
+				<ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+				<Cursor className="bg-primary-400 shadow-primary-400" size={20} trailLength={24} trailScale={0.85} />
+				<SkipLink />
+				{children}
+				<PortalRoot />
+			</body>
+		</html>
+	)
+}
 
 export default RootLayout;
