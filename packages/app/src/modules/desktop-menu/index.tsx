4fc12d3f7d052f720cd2ca50aae663249e996b92
Breakerino <git@breakerino.me>
2025-09-18 23:11:31 +0000

    App: Microinteraction tweaks

--- packages/app/src/modules/desktop-menu/index.tsx
+++ packages/app/src/modules/desktop-menu/index.tsx
@@ -84,7 +84,7 @@ const DesktopMenu: React.FC<DesktopMenuProps> = ({ menuItems, activeMenuItemID, 
 						<Link
 							href={href}
 							className={clsx(
-								'brk-menu-list__link text-lg lg:text-xl no-underline transition-colors',
+								'brk-menu-list__link text-lg lg:text-xl no-underline transition-colors cursor-none',
 								className
 							)}
 							onClick={handleNavigation}
