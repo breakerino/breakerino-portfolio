618a6ad0968136f38e1419b0c2b8389ff0319e84
Breakerino <git@breakerino.me>
2025-09-20 10:11:05 +0000

    App: useTouchDevice - Extend check for `(pointer: coarse)` media query which is more reliable then checking just resize

--- packages/app/src/hooks/useTouchDevice.ts
+++ packages/app/src/hooks/useTouchDevice.ts
@@ -10,15 +10,19 @@ const useTouchDevice = () => {
   const [isTouch, setIsTouch] = React.useState(typeof window !== 'undefined' && ('ontouchstart' in window || navigator.maxTouchPoints > 0));
 
   React.useEffect(() => {
+		const mediaQuery = window.matchMedia('(pointer: coarse)');
+		
     const checkTouch = () => {
       const touch = typeof window !== 'undefined' && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
       setIsTouch(touch);
     };
 
     window.addEventListener('resize', checkTouch);
-
+		mediaQuery.addEventListener('change', checkTouch);
+		
     return () => {
-      window.removeEventListener('resize', checkTouch);
+			window.removeEventListener('resize', checkTouch);
+			mediaQuery.removeEventListener('change', checkTouch);
     };
   }, []);
 
