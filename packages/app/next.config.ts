6822b6ba260b9169794a6f476f589549cae53259
Breakerino <git@breakerino.me>
2025-09-20 10:11:57 +0000

    App: Extend Next.js config - ad `images.deviceSizes`, `images.imageSizes` and `image.qualities`

--- packages/app/next.config.ts
+++ packages/app/next.config.ts
@@ -8,7 +8,7 @@ const remoteURLs = [
 
 const remotePatterns = remoteURLs.map((url) => {
 	const parsedURL = new URL(url);
-	
+
 	return {
 		protocol: parsedURL.protocol.replace(':', '') as 'http' | 'https',
 		hostname: parsedURL.hostname,
@@ -23,6 +23,9 @@ const nextConfig: NextConfig = {
 	basePath: '',
 	assetPrefix: '',
 	images: {
+		deviceSizes: [540, 640, 750, 828, 960, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+		qualities: [25, 50, 75],
 		remotePatterns,
 	},
 };
