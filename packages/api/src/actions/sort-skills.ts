1dc4abcbbc9ac3ff89039a46b801bc42ed970d5d
Breakerino <git@breakerino.me>
2025-09-27 12:02:26 +0000

    Api: Sort skills - Add additional logging

--- packages/api/src/actions/sort-skills.ts
+++ packages/api/src/actions/sort-skills.ts
@@ -7,7 +7,9 @@ import skills from './data/skills';
 // --------------------------------------------------------------------- 
 
 export default async function sortSkills() {
-	for ( const [order, {documentId}] of skills.entries() ) {
+	for ( const [order, {documentId, name}] of skills.entries() ) {
+		strapi.log.info(`Setting skill "${name}" (${documentId}) order to "${order}"...`)
+		
 		await strapi.documents('api::portfolio.skill').update({
 			documentId,
 			data: { order }
